ca218f56ef77c89cfaeade16ab161a318198bef5
hashan-jay <hashanjayy@gmail.com>
2026-09-18 15:50:12 +0530

    Initial Scripts

---
+++ pages/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home_page_view(request):
+    return HttpResponse("<h1>Hello, World!<h1><p>Welcome to my custom Django homepage.</p>")
