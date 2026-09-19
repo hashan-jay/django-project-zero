7e9b7a6fbd13c5a004c4bf8f50a81c38cccf8d0b
hashan-jay <hashanjayy@gmail.com>
2026-09-19 17:53:51 +0530

    Updated Project

--- pages/views.py
+++ pages/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home_page_view(request):
-    return HttpResponse("<h1>Hello, World!<h1><p>Welcome to my custom Django homepage.</p>")
+    return HttpResponse("<h1>Hello, World!<h1><p>Welcome to my custom Django homepage.</p><h2>This is HashJay</h2>")
+
